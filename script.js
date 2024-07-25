d1ddb81b0eb9014d2da847b3715817ef0325fcbd
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-07-25 16:31:01 -0300

    refatorando js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function buscarEMostrarVideos() {
                     <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
                     <h3 class="titulo-video">${video.titulo}</h3>
                     <p class="titulo-canal">${video.descricao}</p>
-                    <p class ="categoria" hidden>${video.categoria}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
                 </div>
             </li>
             `
@@ -28,51 +28,36 @@ async function buscarEMostrarVideos() {
 buscarEMostrarVideos();
 
 const barraDePesquisa = document.querySelector(".pesquisar__input")
-
 barraDePesquisa.addEventListener("input", filtrarPesquisa)
+
 function filtrarPesquisa() {
-    const videos = document.querySelectorAll('.videos__item');
-    const valorFiltro = barraDePesquisa.value.toLowerCase();
+    const videos = document.querySelectorAll('.videos__item')
+    const valorFiltro = barraDePesquisa.value.toLowerCase()
 
     videos.forEach((video) => {
-        const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+        const titulo = video.querySelector('.titulo-video').textContent.toLowerCase()
 
-        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
-    });
+        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block'
+    })
 }
 
-//Código omitido
+const botaoCategoria = document.querySelectorAll(".superior__item")
 
-function filtrarPorCategoria(filtro){
-    const videos = document.querySelectorAll(".videos__item");
-    for(let video of videos){
-        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
-        let valorFiltro = filtro.toLowerCase();
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name")
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria))
+})
 
-        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo'){
-            video.style.display = "none";
-        } else {
-            video.style.display = "block";
-        }
-    }
-}
-
-/*function filtrarPesquisa() {
+function filtrarPorCategoria(filtro) {
     const videos = document.querySelectorAll(".videos__item")
+    const valorFiltro = filtro.toLowerCase();
 
-    if(barraDePesquisa.value != "") {
-        for(let video of videos) {
-            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase()
-            let valorFiltro = barraDePesquisa.value.toLowerCase()
+    videos.forEach((video) => {
+        const categoria = video.querySelector(".categoria").textContent.toLowerCase()
 
-            if(!titulo.includes(valorFiltro)) {
-                video.style.display = "none"
-            } else {
-                video.style.display = "block"
-            }
-        }
-    }else {
-        video.style.display = "block"
-    }
-}*/
+        video.style.display = valorFiltro && valorFiltro !== 'tudo' ? (categoria.includes(valorFiltro) ? 'block' : 'none') : 'block'
+    })
+}
+
+
 
